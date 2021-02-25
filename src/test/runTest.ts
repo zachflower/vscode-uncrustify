c3dca8aa227ea9848c50fc982d3964fdd749b645
Zachary Flower <zach@flower.codes>
2021-02-25 14:15:17 -0700

    update packages

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -3,22 +3,22 @@ import * as path from 'path';
 import { runTests } from 'vscode-test';
 
 async function main() {
-	// try {
+	try {
 		// The folder containing the Extension Manifest package.json
 		// Passed to `--extensionDevelopmentPath`
 		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
 
 		// The path to the extension test script
 		// Passed to --extensionTestsPath
-		const extensionTestsPath = path.resolve(__dirname, './suite/index');
+		const extensionTestsPath = path.resolve(__dirname, './suite');
 
 		// Download VS Code, unzip it and run the integration test
 		await runTests({ extensionDevelopmentPath, extensionTestsPath });
-	// } catch (err) {
-	// 	console.error(err);
-	// 	console.error('Failed to run tests');
-	// 	process.exit(1);
-	// }
+	} catch (err) {
+		console.error(err);
+		console.error('Failed to run tests');
+		process.exit(1);
+	}
 }
 
 main();
