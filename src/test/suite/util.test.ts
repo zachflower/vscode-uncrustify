91473e4136247d320aa16e0173f98c3ed7b615e8
Zachary Flower <zach@flower.codes>
2021-02-25 20:50:17 -0700

    start really fleshing out tests with fixtures, more detail, etc

--- src/test/suite/util.test.ts
+++ src/test/suite/util.test.ts
@@ -1,16 +1,56 @@
-import * as assert from 'assert';
-import { before } from 'mocha';
+import {ok} from 'assert';
+import { before, describe, it } from 'mocha';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
 import * as util from '../../util';
 
-suite('utility functions', () => {
-    test('get platform suffix', () => {
-        assert.strictEqual(util.getPlatformSuffix('linux'), '.linux');
-        assert.strictEqual(util.getPlatformSuffix('darwin'), '.osx');
-        assert.strictEqual(util.getPlatformSuffix('win32'), '.windows');
-        assert.strictEqual(util.getPlatformSuffix('unknown'), undefined);
+describe('utility functions', () => {
+    describe('get platform suffixes', () => {
+        it('should properly parse each context', () => {
+            ok(util.getPlatformSuffix('linux') === '.linux');
+            ok(util.getPlatformSuffix('darwin') === '.osx');
+            ok(util.getPlatformSuffix('win32') === '.windows');
+            ok(util.getPlatformSuffix('unknown') === undefined);
+        });
+    });
+
+    describe('get extension config', () => {
+        describe('default workspace folder', () => {
+            let config;
+
+            before(() => {
+                config = util.getExtensionConfig();
+            });
+
+            it('should retrieve the linux config', () => {
+                ok(config.get("configPath.linux") === 'uncrustify.linux.cfg');
+            });
+
+            it('should fall back to the workspace config', () => {
+                ok(config.get("configPath.windows") === 'uncrustify.cfg');
+            });
+        });
+
+        describe('passed workspace folder', () => {
+            let config;
+
+            before(() => {
+                const workspaceFolder = vscode.workspace.workspaceFolders.find((folder) => {
+                    return folder.name === "windows";
+                });
+
+                config = util.getExtensionConfig(workspaceFolder.uri);
+            });
+
+            it('should retreve the windows config', () => {
+                ok(config.get("configPath.windows") === 'uncrustify.windows.cfg');
+            });
+
+            it('should fall back to the workspace config', () => {
+                ok(config.get("configPath.linux") === 'uncrustify.cfg');
+            });
+        });
     });
 });
